5c2b075d36f1161d6f6db46b66a805f1a82ee18b
chi <chi15036@gmail.com>
2016-12-29 11:59:10 +0800

    Add expressions

--- src/components/EntityDetection/EntityInfo.js
+++ src/components/EntityDetection/EntityInfo.js
@@ -18,30 +18,56 @@ export default class EntityInfo extends React.Component {
     init(props) {
       let intent = props.entityInfo.entities.intent ?
       props.entityInfo.entities.intent[0].value : ""
-      let entities = [{"id": "intent", "values": [{"value": intent, "expressions":[props.sentence]}]}]
+      let entities = [{"id": "intent", "lookups": ["trait"], "values": [{"value": intent, "expressions":[props.sentence]}]}]
       delete props.entityInfo.entities.intent
       Object.keys(props.entityInfo.entities).map((entityName, idx) => {
         entities.push({"id": entityName, "values":
-        [{"value": props.entityInfo.entities[entityName][0].value}]})
+        [{"value": props.entityInfo.entities[entityName][0].value,
+        "expressions":[props.entityInfo.entities[entityName][0].value]}]})
       })
       this.state = {"entities": entities}
       this.wordOption = props.sentence.split(" ")
     }
 
     handleChange(idx, field, e) {
+      console.log(e.target.value)
       let entities = this.state.entities
       if(field === 'id') {
         entities[idx].id = e.target.value
       }
       if(field === 'value') {
         entities[idx].values[0].value = e.target.value
+        if(idx !== 0) { // !== 'intent'
+          entities[idx].values[0].expressions[0] = e.target.value
+        }
       }
+      this.setState({"entities": entities})
+      console.log(this.state.entities)
+    }
+
+    addSlot() {
+      let entities = this.state.entities
+      entities.push({"id": "", "values": [{"value": this.wordOption[0],
+      "expressions":[this.wordOption[0]]}]})
+      this.setState({"entities": entities})
+    }
+
+    deleteSlot(idx) {
+      let entities = this.state.entities
+      entities.splice(idx, 1)
       this.setState({"entities": entities})
     }
 
     render() {
         return (
           <div>
+            <FormGroup>
+              <Col sm={2}>
+                <Button onClick={this.addSlot.bind(this)}>
+                  新增Slot
+                </Button>
+              </Col>
+            </FormGroup>
             {this.state.entities.map((entity, idx) => {
               return <FormGroup key={idx}>
                 <Col sm={4}>
@@ -60,6 +86,13 @@ export default class EntityInfo extends React.Component {
                     }) : ""}
                   </FormControl>
                 </Col>
+                {entity.id !== 'intent' ?
+                <Col sm={2}>
+                  <Button onClick={this.deleteSlot.bind(this, idx)}>
+                    刪除
+                  </Button>
+                </Col>
+                : ""}
               </FormGroup>
             })}
             <FormGroup>
